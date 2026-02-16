e58ecf5c1f8e370647c6160f96e1842874491df2
EDUfrms <edufernandezramos97@gmail.com>
2026-02-16 19:50:35 +0100

    Configure base path for GitHub Pages deployment
    Set base path for deployment on GitHub Pages.

--- vite.config.ts
+++ vite.config.ts
@@ -5,6 +5,7 @@ import react from '@vitejs/plugin-react';
 export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
     return {
+      base: "/prueba-3/",   // 👈 NECESARIO para GitHub Pages
       server: {
         port: 3000,
         host: '0.0.0.0',
